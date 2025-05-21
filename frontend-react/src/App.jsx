04d01db3d56116feb5bd31b808ef1c8c34782a05
stanfc <stanfc@NB>
2025-05-21 12:42:35 +0800

    add route to each page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route , useParams} from "react-router-dom";
 import OAuthRedirect from "./components/OAuthRedirect";
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { fetchCalendars } from "./services/api";
+import CalendarDetailPage from "./pages/CalendarDetailPage";
+import GroupsPage from "./pages/GroupsPage";
+
+
+const CalendarDetailPageWrapper = () => {
+  const { calendarId } = useParams();
+  const token = localStorage.getItem("token");
+
+  return <CalendarDetailPage calendarId={calendarId} token={token} />;
+};
+
+const GroupsPageWrapper = () => {
+  const token = localStorage.getItem("token");
+
+  return <GroupsPage token={token} />
+}
+
+const PollPageWrapper = () => {
+  const token = localStorage.getItem("token");
+
+  return <PollsPage token={token} />
+}
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
@@ -15,6 +37,9 @@ const App = () => {
         <Route path="/login" element={<LoginPage setToken={setToken} />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/oauth" element={<OAuthRedirect setToken={setToken} />} />
+        <Route path="/calendar/:calendarId" element={<CalendarDetailPageWrapper />} />
+        <Route path="/group" element={<GroupsPageWrapper />}/>
+        <Route path="/poll" element={<PollPageWrapper />}/>
       </Routes>
     </Router>
   );
